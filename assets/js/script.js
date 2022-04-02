00fcc08cc301cfb30b15af5a13f0abdaedc4e34a
Oindrila Talukder <oindrilatalukder.ot@gmail.com>
2022-04-02 13:15:47 -0500

    add script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,32 @@
 // Define common variable
+var saveButton = $(".saveButton");
 
 // Display current day at top
+$("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
 
 // tasks color change past, present, futture
+function timeDisplay() {
+    var Time = moment().hours();
+
+    $(".time-block").each(function() {
+        var presentTime = parseInt($(this).attr("id"));
+
+        if (presentTime > Time) {
+            $(this).addClass("future");
+        } else if (presentTime === Time) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+            
+        }
+    
+    })
+};
+
+
+timeDisplay();
+
+
+
 
 //saving tasks to local storage
